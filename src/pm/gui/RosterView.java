a4f2175f7560a7df33296f35d0fee9ddd415538e
sajidkamal <sajidkamal@10.120.253.224>
2016-07-01 14:23:26 -0400

    Salary Tab added on roster view.

--- src/pm/gui/RosterView.java
+++ src/pm/gui/RosterView.java
@@ -6,22 +6,25 @@
 package pm.gui;
 
 
+import java.text.NumberFormat;
+import java.util.Locale;
 import javafx.beans.property.ReadOnlyObjectWrapper;
-import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableColumn.CellDataFeatures;
+import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-import javafx.util.Callback;
 import pm.data.DataManager;
 import pm.data.Player;
 import pm.data.Team;
@@ -36,21 +39,31 @@ public class RosterView extends Page {
 
     ObservableList<Team> teams;
     ObservableList<Player> players;
+    NumberFormat fmt = NumberFormat.getCurrencyInstance(Locale.US);
     VBox rosterPane;
     Pane workPane;
     AppTemplate app;
     ComboBox teamPicker;
     TableView rosterTable;
     TableView statsTable;
+    TableView salaryTable;
     TabPane rosterTabs;
     int currentTeamIndex=0; // this is to keep track of which team page you are on, so if you reload this page, it will start off at the same page.
+    Label teamName;
+    int year;
+    
+    Label totalSalary;
+    Label capSpaceAvailable;
     
     public RosterView(AppTemplate app, Pane workPane){
         this.app=app;
         this.workPane=workPane;
         
+      
+        
         DataManager data= (DataManager) app.getDataComponent();
         teams= data.getTeamsArray();
+        year=data.getYear();
         
         rosterPane= new VBox();
         rosterTabs= new TabPane();
@@ -61,6 +74,7 @@ public class RosterView extends Page {
             public void changed(ObservableValue ov, Team t, Team t1) {                
                            loadRoster(t1);
                            currentTeamIndex=teamPicker.getItems().indexOf(t1);
+                           teamName.setText(t1.toString());
             }    
         });
 
@@ -71,6 +85,12 @@ public class RosterView extends Page {
         
         statsTable= new TableView();
         initStatsTable();
+        
+        salaryTable= new TableView();
+        initSalaryTable();
+        
+        
+        
         
 
          Tab rosters= new Tab();
@@ -83,9 +103,14 @@ public class RosterView extends Page {
         stats.setClosable(false);
         stats.setContent(statsTable);
         
-         
-         rosterTabs.getTabs().addAll(rosters,stats);
-       rosterPane.getChildren().addAll(teamPicker,rosterTabs);
+        Tab salary= new Tab();
+        salary.setText("Salaries");
+        salary.setClosable(false);
+        salary.setContent(salaryTable);
+        
+         teamName = new Label();
+         rosterTabs.getTabs().addAll(rosters,stats,salary);
+       rosterPane.getChildren().addAll(teamName,teamPicker,rosterTabs);
        
        initStyles();
        
@@ -103,9 +128,10 @@ public class RosterView extends Page {
     public void loadRoster(Team t){
         rosterTable.setItems(t.getPlayers());
         statsTable.setItems(t.getPlayers());
+        salaryTable.setItems(t.getPlayers());
     }
    
-    public void initRosterTable(){
+    private void initRosterTable(){
         
         TableColumn<Player,String> playerName= new TableColumn("Name");
         playerName.setSortable(true);
@@ -170,10 +196,25 @@ public class RosterView extends Page {
 
         
     rosterTable.getColumns().addAll(playerName,playerPos,playerAge,playerHeight,playerWeight,playerExp,playerInRating,playerMidRating,playerThreeRating,playerHandleRating,playerPostRating,playerPerimeterRating,playerReboundRating);
+    
+    rosterTable.setRowFactory( tv -> {
+    TableRow<Player> row = new TableRow<>();
+    row.setOnMouseClicked(event -> {
+        if (event.getClickCount() == 2 && (! row.isEmpty()) ) {
+           Player rowData = row.getItem();
+            PlayerInfoSingleton playerPage = PlayerInfoSingleton.getSingleton();
+            playerPage.show(rowData,rosterTable);
+        }
+    });
+    return row ;
+});
+    
     }
     
     
-    public void initStatsTable(){
+    private void initStatsTable(){
+        
+        
          TableColumn<Player,String> playerName= new TableColumn("Name");
         playerName.setSortable(true);
         playerName.setCellValueFactory(
@@ -269,12 +310,62 @@ public class RosterView extends Page {
         
     statsTable.getColumns().addAll(playerName,playerPos,gamesPlayed,minutesPlayed,fgMade,fgAttempt,fgPercent,threeMade,threeAttempted,threePercent,ftm,fta,ftper,or,dr,tot,ast,blk,stl,pf,to,pts);
     }
+    
+    private void initSalaryTable(){
+         TableColumn<Player,String> playerName= new TableColumn("Name");
+        playerName.setSortable(true);
+        playerName.setCellValueFactory(
+        new PropertyValueFactory<>("Name"));
+        playerName.setMinWidth(120);
+        
+         TableColumn<Player,String> playerPos= new TableColumn("Pos");
+        playerPos.setSortable(true);
+        playerPos.setCellValueFactory(
+            new PropertyValueFactory<>("Pos"));
+        
+       TableColumn<Player,Integer> year1= new TableColumn(String.valueOf(year));
+       year1.setSortable(false);
+       year1.setCellValueFactory((CellDataFeatures<Player, Integer> p) -> new ReadOnlyObjectWrapper(fmt.format(p.getValue().getContract().getYear1()).replaceAll("\\.00", "")));
+       
+       TableColumn<Player,Integer> year2= new TableColumn(String.valueOf(year+1));
+       year2.setSortable(false);
+       year2.setCellValueFactory((CellDataFeatures<Player, Integer> p) -> new ReadOnlyObjectWrapper(fmt.format(p.getValue().getContract().getYear2()).replaceAll("\\.00", "")));
+        
+       TableColumn<Player,Integer> year3= new TableColumn(String.valueOf(year+2));
+       year3.setSortable(false);
+       year3.setCellValueFactory((CellDataFeatures<Player, Integer> p) -> new ReadOnlyObjectWrapper(fmt.format(p.getValue().getContract().getYear3()).replaceAll("\\.00", "")));
+       
+       TableColumn<Player,Integer> year4= new TableColumn(String.valueOf(year+3));
+       year4.setSortable(false);
+       year4.setCellValueFactory((CellDataFeatures<Player, Integer> p) -> new ReadOnlyObjectWrapper(fmt.format(p.getValue().getContract().getYear4()).replaceAll("\\.00", "")));
+       
+       TableColumn<Player,Integer> year5= new TableColumn(String.valueOf(year+4));
+       year5.setSortable(false);
+       year5.setCellValueFactory((CellDataFeatures<Player, Integer> p) -> new ReadOnlyObjectWrapper(fmt.format(p.getValue().getContract().getYear5()).replaceAll("\\.00", "")));
+       
+       TableColumn<Player,Integer> year6= new TableColumn(String.valueOf(year+5));
+       year6.setSortable(false);
+       year6.setCellValueFactory((CellDataFeatures<Player, Integer> p) -> new ReadOnlyObjectWrapper(fmt.format(p.getValue().getContract().getYear6()).replaceAll("\\.00", "")));
+       
+       TableColumn<Player,Integer> year7= new TableColumn(String.valueOf(year+6));
+       year7.setSortable(false);
+       year7.setCellValueFactory((CellDataFeatures<Player, Integer> p) -> new ReadOnlyObjectWrapper(fmt.format(p.getValue().getContract().getYear7()).replaceAll("\\.00", "")));
+       
+       TableColumn<Player,Integer> total= new TableColumn("Total");
+       total.setSortable(false);
+       total.setCellValueFactory((CellDataFeatures<Player, Integer> p) -> new ReadOnlyObjectWrapper(fmt.format(p.getValue().getContract().getTotal()).replaceAll("\\.00", "")));
+        
+        salaryTable.getColumns().addAll(playerName,playerPos,year1,year2,year3,year4,year5,year6,year7,total);
+    }
+    
+    
     public void initStyles(){
         rosterPane.prefHeightProperty().bind(workPane.heightProperty());
         rosterPane.prefWidthProperty().bind(workPane.widthProperty());
         rosterPane.getStyleClass().add("test");
         rosterPane.setSpacing(20);
         rosterPane.setAlignment(Pos.CENTER);
+        
        
     }
     
